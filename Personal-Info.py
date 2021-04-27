2d0e21b35d99415d3826d84733d3dfbcc1941a10
KeansErol <82988408+KeansErol@users.noreply.github.com>
2021-04-27 21:22:24 +0800

    Update Personal-Info.py

--- Personal-Info.py
+++ Personal-Info.py
@@ -1,3 +1,8 @@
-print("Hi my name is Keans Erol Austria")
-print("I am 20 years old, I live in Pila, Laguna")
-print("Second year student in Information Technology at DLSUD")
+firstName = input ("What is your first name? ")
+lastName = input ("What is your last name? " )
+location = input ("What is your location? ")
+age = input ("What is your age? ")
+space = " "
+
+print("Hi"+space+firstName+space+lastName+"!"+space+"Your location is"+space+location+space+"and you are"+space+age+space+"years old.")
+
